916a0cfd42a7259e0431adc3697b373fef9e4eaf
Karl Woditsch <k.woditsch@gmail.com>
2013-01-15 19:50:27 +0100

    Fight implement

--- src/brutes/game/Fight.java
+++ src/brutes/game/Fight.java
@@ -9,5 +9,18 @@ package brutes.game;
  * @author Karl
  */
 public class Fight {
-    
+    private Character me;
+    private Character chalenger;
+
+    public Fight(Character me, Character chalenger) {
+        this.me = me;
+        this.chalenger = chalenger;
+    }
+
+    public Character getMe() {
+        return me;
+    }
+    public Character getChalenger() {
+        return chalenger;
+    }
 }
