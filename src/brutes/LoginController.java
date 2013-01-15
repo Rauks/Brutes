ecea859c308b3a710e2dd941691685f7bf13a446
Karl Woditsch <k.woditsch@gmail.com>
2013-01-15 22:21:11 +0100

    login windows controller

--- src/brutes/LoginController.java
+++ src/brutes/LoginController.java
@@ -5,9 +5,25 @@ package brutes;
  * and open the template in the editor.
  */
 
+import brutes.user.Session;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.concurrent.Worker.State;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -15,7 +31,67 @@ import javafx.fxml.Initializable;
  * @author Karl
  */
 public class LoginController implements Initializable {
+    private Session session;
+    
+    @FXML
+    private Button connexion;
+    @FXML
+    private TextField login;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private ProgressIndicator loading;
+    
+    @FXML
+    private void handleKeyPressed(KeyEvent e){
+        Logger.getLogger(LoginController.class.getName()).log(Level.INFO, "Handle");
+        KeyCode key = e.getCode();
+        if(key == KeyCode.ENTER){
+            this.login();
+        }
+    }
+    @FXML
+    private void handleConnexionAction(ActionEvent e){
+        Logger.getLogger(LoginController.class.getName()).log(Level.INFO, "Handle");
+        this.login();
+    }
+    
+    private synchronized void login(){
+        Logger.getLogger(LoginController.class.getName()).log(Level.INFO, "Login");
+        
+        this.loading.setVisible(true);
+        this.login.setDisable(true);
+        this.password.setDisable(true);
+        this.connexion.setDisable(true);
+        
+        final LoginTask loginTask = new LoginTask();
+        new Thread(loginTask).start();
+        loginTask.stateProperty().addListener(new ChangeListener<Worker.State>() {
+            @Override
+            public void changed(ObservableValue<? extends State> observable, State oldValue, State newState) {
+                if(newState == Worker.State.SUCCEEDED){
+                    loginTask.cancel();
+                    session = loginTask.getSession();
+                    Logger.getLogger(LoginController.class.getName()).log(Level.INFO, session.getToken());
+                    this.reactiveLogin();
+                }
+                else if(newState == Worker.State.FAILED){
+                    loginTask.cancel();
+                    Logger.getLogger(LoginController.class.getName()).log(Level.INFO, "Thread failed");
+                    this.reactiveLogin();
+                }
+            }   
 
+            private void reactiveLogin() {
+                Logger.getLogger(LoginController.class.getName()).log(Level.INFO, "Reactive login");
+                login.setDisable(false);
+                password.setDisable(false);
+                connexion.setDisable(false);
+                loading.setVisible(false);
+            }
+        });
+    }
+   
     /**
      * Initializes the controller class.
      */
