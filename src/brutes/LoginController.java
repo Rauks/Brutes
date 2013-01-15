7245a2c904ff2b7d475c0cfcd65553c5118818ff
Karl Woditsch <k.woditsch@gmail.com>
2013-01-15 18:57:49 +0100

    Base class creation

---
+++ src/brutes/LoginController.java
@@ -0,0 +1,26 @@
+package brutes;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Karl
+ */
+public class LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
