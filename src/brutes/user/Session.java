108b9d5e78c78f2e0803498af3377741045c09a8
Karl Woditsch <k.woditsch@gmail.com>
2013-01-15 19:03:16 +0100

    token in Session

--- src/brutes/user/Session.java
+++ src/brutes/user/Session.java
@@ -9,5 +9,13 @@ package brutes.user;
  * @author Karl
  */
 public class Session {
-    
+    private String token;
+
+    public Session(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
