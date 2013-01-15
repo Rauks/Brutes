64bfbcc167be556b1af1176718b199296bc4a840
Karl Woditsch <k.woditsch@gmail.com>
2013-01-15 19:42:16 +0100

    Bonus implementation

--- src/brutes/game/Bonus.java
+++ src/brutes/game/Bonus.java
@@ -4,10 +4,45 @@
  */
 package brutes.game;
 
+import brutes.game.media.BonusImage;
+
 /**
  *
  * @author Karl
  */
 public class Bonus {
-    
+    private int id;
+    private String name;
+    private int level;
+    private int strength;
+    private int speed;
+    private BonusImage image;
+
+    public Bonus(int id, String name, int level, int strength, int speed, BonusImage image) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.strength = strength;
+        this.speed = speed;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public BonusImage getImage() {
+        return image;
+    }
 }
