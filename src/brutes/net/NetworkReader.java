87adcc6d0ab8d26873c831621d0488c3b39ca108
Karl Woditsch <k.woditsch@gmail.com>
2013-01-16 11:24:32 +0100

    implement reader base types

--- src/brutes/net/NetworkReader.java
+++ src/brutes/net/NetworkReader.java
@@ -4,7 +4,11 @@
  */
 package brutes.net;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,5 +19,50 @@ class NetworkReader {
 
     public NetworkReader(InputStream is) {
         this.is = is;
+    }
+    
+    public int readDiscriminant(){
+        try {
+            byte[] b = new byte[Protocol.SIZE_DISCRININANT];
+            this.is.read(b);
+            ByteBuffer bb = ByteBuffer.wrap(b);
+            return bb.getInt();
+        } catch (IOException ex) {
+            Logger.getLogger(NetworkReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Protocol.R_ERROR;
+    }
+    public short readShortInt(){
+        try {
+            byte[] b = new byte[Protocol.SIZE_SHORTINT];
+            this.is.read(b);
+            ByteBuffer bb = ByteBuffer.wrap(b);
+            return bb.getShort();
+        } catch (IOException ex) {
+            Logger.getLogger(NetworkReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+    public long readLongInt(){
+        try {
+            byte[] b = new byte[Protocol.SIZE_LONGINT];
+            this.is.read(b);
+            ByteBuffer bb = ByteBuffer.wrap(b);
+            return bb.getLong();
+        } catch (IOException ex) {
+            Logger.getLogger(NetworkReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+    public String readString(){
+        try {
+            short length = this.readShortInt();
+            byte[] b = new byte[length];
+            this.is.read(b);
+            return new String(b, "UTF-8");
+        } catch (IOException ex) {
+            Logger.getLogger(NetworkReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
