163ddcaab0bb6d74bed36486e5c2f0dfe5ee2ed5
Karl Woditsch <k.woditsch@gmail.com>
2013-01-16 09:04:21 +0100

    network writer

---
+++ src/brutes/net/NetworkWriter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brutes.net;
+
+import java.io.OutputStream;
+
+/**
+ *
+ * @author Karl
+ */
+class NetworkWriter {
+    private OutputStream os;
+
+    public NetworkWriter(OutputStream os) {
+        this.os = os;
+    }
+    
+}
