c1dd205b6d9c78feff3d179b3be79aabf51c18f1
Karl Woditsch <k.woditsch@gmail.com>
2013-01-16 09:04:16 +0100

    Network reader

---
+++ src/brutes/net/NetworkReader.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brutes.net;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author Karl
+ */
+class NetworkReader {
+    private InputStream is;
+
+    public NetworkReader(InputStream is) {
+        this.is = is;
+    }
+}
