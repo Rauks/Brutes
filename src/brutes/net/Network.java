db7645554fa614cd6a1f6da069bfbc40ddb4278b
Karl Woditsch <k.woditsch@gmail.com>
2013-01-17 00:21:46 +0100

    clients > server messages sender in Network
    not implemented :
    - get datas from image
    - errors handlers (throw InvalidResponseException in any error cases)

--- src/brutes/net/Network.java
+++ src/brutes/net/Network.java
@@ -4,16 +4,11 @@
  */
 package brutes.net;
 
-import brutes.ScenesContext;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import brutes.game.Bonus;
+import brutes.game.Character;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,7 +27,7 @@ public class Network{
         this.writer = new NetworkWriter(this.connection.getOutputStream());
     }
     
-    public String sendLogin(String user, String password) throws Exception{
+    public String sendLogin(String user, String password) throws IOException, ErrorResponseException, InvalidResponseException{
         this.writer.writeDiscriminant(Protocol.D_LOGIN)
                 .writeString(user)
                 .writeString(password)
@@ -42,14 +37,145 @@ public class Network{
             case Protocol.R_LOGIN_SUCCESS:
                 return this.reader.readString();
             case Protocol.ERROR_LOGIN_NOT_FOUND:
-                throw new Exception("Invalid login");
+                throw new ErrorResponseException("Invalid login");
             case Protocol.ERROR_WRONG_PASSWORD:
-                throw new Exception("Wrong password");
+                throw new ErrorResponseException("Wrong password");
             default:
-                throw new Exception("Invalid server response");
+                throw new InvalidResponseException();
         }
     }
-    public void readLogin(){//Test purpose
+    public void sendLogout(String token) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(Protocol.D_LOGOUT)
+                .writeString(token)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_LOGOUT_SUCCESS:
+                break;
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    
+    public void sendCreateCharacter(String token, String name) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(Protocol.D_CREATE_CHARACTER)
+                .writeString(token)
+                .writeString(name)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_ACTION_SUCCESS:
+                break;
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    public void sendUpdateCharacter(String token, String name) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(Protocol.D_UPDATE_CHARACTER)
+                .writeString(token)
+                .writeString(name)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_ACTION_SUCCESS:
+                break;
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    public void sendDeleteCharacter(String token) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(Protocol.D_DELETE_CHARACTER)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_ACTION_SUCCESS:
+                break;
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    
+    private int sendGetCharacterId(byte getIdDiscriminant) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(getIdDiscriminant)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_CHARACTER:
+                return this.reader.readLongInt();
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    public int sendGetMyCharacterId(String token) throws IOException, InvalidResponseException{
+        return this.sendGetCharacterId(Protocol.D_GET_MY_CHARACTER_ID);
+    }
+    public int sendGetChallengerCharacterId(String token) throws IOException, InvalidResponseException{
+        return this.sendGetCharacterId(Protocol.D_GET_CHALLENGER_CHARACTER_ID);
+    }
+    
+    private boolean sendFight(byte fightType) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(fightType)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_FIGHT_RESULT:
+                return this.reader.readBoolean();
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    public boolean sendCheatFightWin(String token) throws IOException, InvalidResponseException{
+        return sendFight(Protocol.D_CHEAT_FIGHT_WIN);
+    }
+    public boolean sendCheatFightLoose(String token) throws IOException, InvalidResponseException{
+        return sendFight(Protocol.D_CHEAT_FIGHT_LOOSE);
+    }
+    public boolean sendCheatFightRandom(String token) throws IOException, InvalidResponseException{
+        return sendFight(Protocol.D_CHEAT_FIGHT_RANDOM);
+    }
+    public boolean sendDoFight(String token) throws IOException, InvalidResponseException{
+        return sendFight(Protocol.D_DO_FIGHT);
+    }
+    public Character getDataCharacter(int id) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(Protocol.D_GET_CHARACTER)
+                .writeLongInt(id)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_DATA_CHARACTER:
+                int chId = this.reader.readLongInt();
+                String name = this.reader.readString();
+                short level = this.reader.readShortInt();
+                short life = this.reader.readShortInt();
+                short strength = this.reader.readShortInt();
+                short speed = this.reader.readShortInt();
+                int imageID = this.reader.readShortInt();
+                ArrayList<Integer> bonusesID = (ArrayList<Integer>) this.reader.readArray();
+                return new Character(chId, name, level, life, strength, speed, imageID, bonusesID);
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    public Bonus getDataBonus(int id) throws IOException, InvalidResponseException{
+        this.writer.writeDiscriminant(Protocol.D_GET_BONUS)
+                .writeLongInt(id)
+                .send();
+        this.reader.readMessageSize();
+        switch(this.reader.readDiscriminant()){
+            case Protocol.R_DATA_BONUS:
+                int boId = this.reader.readLongInt();
+                String name = this.reader.readString();
+                short level = this.reader.readShortInt();
+                short strength = this.reader.readShortInt();
+                short speed = this.reader.readShortInt();
+                int imageID = this.reader.readShortInt();
+                return new Bonus(boId, name, level, strength, speed, imageID);
+            default:
+                throw new InvalidResponseException();
+        }
+    }
+    
+    public void readLogin() throws IOException{//Test purpose
         System.out.println(this.reader.readMessageSize());
         System.out.println(this.reader.readDiscriminant());
         String login = this.reader.readString();
