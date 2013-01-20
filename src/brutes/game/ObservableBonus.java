a83494e786779f2b69bc26ab12ea970cc5acfe12
Karl Woditsch <k.woditsch@gmail.com>
2013-01-20 11:28:54 +0100

    ObservableBonus loading datas & base

--- src/brutes/game/ObservableBonus.java
+++ src/brutes/game/ObservableBonus.java
@@ -4,10 +4,37 @@
  */
 package brutes.game;
 
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+
 /**
  *
  * @author Karl
  */
 public class ObservableBonus {
+    private ReadOnlyIntegerWrapper id;
+    private ReadOnlyStringWrapper name;
+    private ReadOnlyIntegerWrapper level;
+    private ReadOnlyIntegerWrapper strength;
+    private ReadOnlyIntegerWrapper speed;
+    private ReadOnlyIntegerWrapper imageID;
+    
+    public ObservableBonus(){
+        this.id = new ReadOnlyIntegerWrapper();
+        this.name = new ReadOnlyStringWrapper();
+        this.level = new ReadOnlyIntegerWrapper();
+        this.strength = new ReadOnlyIntegerWrapper();
+        this.speed = new ReadOnlyIntegerWrapper();
+        this.imageID = new ReadOnlyIntegerWrapper();
+    }
+    
+    public void loadBonus(Bonus b){
+        this.id.set(b.getId());
+        this.name.set(b.getName());
+        this.level.set(b.getLevel());
+        this.strength.set(b.getStrength());
+        this.speed.set(b.getSpeed());
+        this.imageID.set(b.getImage());
+    }
     
 }
