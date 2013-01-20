16cf55b3939c5c134518660a7671cbeef9824ad3
Karl Woditsch <k.woditsch@gmail.com>
2013-01-20 17:49:33 +0100

    array writting

--- src/brutes/net/NetworkWriter.java
+++ src/brutes/net/NetworkWriter.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,8 +32,12 @@ public class NetworkWriter {
     }
     
     public NetworkWriter writeDiscriminant(byte value){
+        this.writeByte(value);
+        return this;
+    }
+    public NetworkWriter writeByte(byte value){
         try {
-            this.baos.write(ByteBuffer.allocate(Protocol.SIZE_DISCRININANT).put(value).array());
+            this.baos.write(ByteBuffer.allocate(Protocol.SIZE_BYTE).put(value).array());
         } catch (IOException ex) {
             Logger.getLogger(NetworkWriter.class.getName()).log(Level.WARNING, null, ex);
         }
@@ -69,6 +74,38 @@ public class NetworkWriter {
             this.baos.write(ByteBuffer.allocate(Protocol.SIZE_BOOLEAN).put(bool).array());
         } catch (IOException ex) {
             Logger.getLogger(NetworkWriter.class.getName()).log(Level.WARNING, null, ex);
+        }
+        return this;
+    }
+    public NetworkWriter writeBooleanArray(boolean[] array){
+        this.writeShortInt((short)array.length);
+        this.writeByte(Protocol.TYPE_BOOLEAN);
+        for(int i = 0; i < array.length; i++){
+            this.writeBoolean(array[i]);
+        }
+        return this;
+    }
+    public NetworkWriter writeLongIntArray(int[] array){
+        this.writeShortInt((short)array.length);
+        this.writeByte(Protocol.TYPE_LONG);
+        for(int i = 0; i < array.length; i++){
+            this.writeLongInt(array[i]);
+        }
+        return this;
+    }
+    public NetworkWriter writeShortIntArray(short[] array){
+        this.writeShortInt((short)array.length);
+        this.writeByte(Protocol.TYPE_SHORT);
+        for(int i = 0; i < array.length; i++){
+            this.writeShortInt(array[i]);
+        }
+        return this;
+    }
+    public NetworkWriter writeStringArray(String[] array){
+        this.writeShortInt((short)array.length);
+        this.writeByte(Protocol.TYPE_STRING);
+        for(int i = 0; i < array.length; i++){
+            this.writeString(array[i]);
         }
         return this;
     }
