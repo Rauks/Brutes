5b7a6579bea5a04b72a9ffe16bed65034afd3153
Karl Woditsch <k.woditsch@gmail.com>
2013-01-20 17:56:28 +0100

    testServer responses

--- src/brutes/net/server/NetworkLocalTestServer.java
+++ src/brutes/net/server/NetworkLocalTestServer.java
@@ -8,8 +8,6 @@ import brutes.net.Network;
 import brutes.net.Protocol;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -33,53 +31,65 @@ public class NetworkLocalTestServer extends Network{
                 this.readCheatFightWin();
                 break;
             case Protocol.D_CREATE_CHARACTER:
+                this.readCreateCharacter();
+                break;
+            case Protocol.D_UPDATE_CHARACTER:
+                this.readUpdateCharacter();
                 break;
             case Protocol.D_DELETE_CHARACTER:
+                this.readDeleteCharacter();
                 break;
             case Protocol.D_DO_FIGHT:
                 this.readDoFight();
                 break;
             case Protocol.D_GET_BONUS:
+                this.readDataBonus();
                 break;
             case Protocol.D_GET_CHALLENGER_CHARACTER_ID:
+                this.readGetChallengerCharacterId();
                 break;
             case Protocol.D_GET_CHARACTER:
+                this.readDataCharacter();
                 break;
             case Protocol.D_GET_MY_CHARACTER_ID:
+                this.readGetMyCharacterId();
                 break;
             case Protocol.D_LOGIN:
                 this.readLogin();
                 break;
             case Protocol.D_LOGOUT:
-                break;
-            case Protocol.D_UPDATE_CHARACTER:
+                this.readLogout();
                 break;
             default:
                 this.getWriter().writeDiscriminant(Protocol.ERROR_SRLY_WTF).send();
         }
     }
     
-    private void readCheatFightWin(){
+    private void readCheatFightWin() throws IOException{
+        this.getReader().readString();
         this.getWriter().writeDiscriminant(Protocol.R_FIGHT_RESULT)
                 .writeBoolean(true)
                 .send();
     }
-    private void readCheatFightLoose(){
+    private void readCheatFightLoose() throws IOException{
+        this.getReader().readString();
         this.getWriter().writeDiscriminant(Protocol.R_FIGHT_RESULT)
                 .writeBoolean(false)
                 .send();
     }
-    private void readCheatFightRandom(){
+    private void readCheatFightRandom() throws IOException{
+        this.getReader().readString();
         this.getWriter().writeDiscriminant(Protocol.R_FIGHT_RESULT)
                 .writeBoolean(Math.random() < 0.5)
                 .send();
     }
-    private void readDoFight(){
+    private void readDoFight() throws IOException{
+        this.getReader().readString();
         this.getWriter().writeDiscriminant(Protocol.R_FIGHT_RESULT)
                 .writeBoolean(true)
                 .send();
     }
-    
+     
     private void readLogin() throws IOException{
         String login = this.getReader().readString();
         String password = this.getReader().readString();
@@ -98,5 +108,64 @@ public class NetworkLocalTestServer extends Network{
                     .writeString(token)
                     .send();
         }
+    }
+    private void readLogout() throws IOException{
+        this.getReader().readString();
+        this.getWriter().writeDiscriminant(Protocol.R_LOGOUT_SUCCESS)
+                .send();
+    }
+
+    private void readCreateCharacter() throws IOException {
+        this.getReader().readString();
+        this.getReader().readString();
+        this.getWriter().writeDiscriminant(Protocol.R_ACTION_SUCCESS)
+                .send();
+    }
+    private void readUpdateCharacter() throws IOException {
+        this.getReader().readString();
+        this.getReader().readString();
+        this.getWriter().writeDiscriminant(Protocol.R_ACTION_SUCCESS)
+                .send();
+    }
+    private void readDeleteCharacter() throws IOException {
+        this.getReader().readString();
+        this.getWriter().writeDiscriminant(Protocol.R_ACTION_SUCCESS)
+                .send();
+    }
+
+    private void readDataBonus() {
+        this.getWriter().writeDiscriminant(Protocol.R_DATA_BONUS)
+                .writeLongInt(1)
+                .writeString("Hache")
+                .writeShortInt((short)2)
+                .writeShortInt((short)5)
+                .writeShortInt((short)7)
+                .writeLongInt(2)
+                .send();
+    }
+
+    private void readDataCharacter() {
+        this.getWriter().writeDiscriminant(Protocol.R_DATA_CHARACTER)
+                .writeLongInt(1)
+                .writeString("Rauks")
+                .writeShortInt((short)15)
+                .writeShortInt((short)17)
+                .writeShortInt((short)6)
+                .writeLongInt(1)
+                .writeLongIntArray(new int[]{1, 1})
+                .send();
+    }
+
+    private void readGetChallengerCharacterId() throws IOException {
+        this.getReader().readString();
+        this.getWriter().writeDiscriminant(Protocol.R_CHARACTER)
+                .writeLongInt(1)
+                .send();
+    }
+    private void readGetMyCharacterId() throws IOException {
+        this.getReader().readString();
+        this.getWriter().writeDiscriminant(Protocol.R_CHARACTER)
+                .writeLongInt(2)
+                .send();
     }
 }
