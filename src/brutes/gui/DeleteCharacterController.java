3eb89ee9332cf767781724eed14d5a8f177777da
Karl Woditsch <k.woditsch@gmail.com>
2013-01-21 14:03:24 +0100

    character delete controller handlers

--- src/brutes/gui/DeleteCharacterController.java
+++ src/brutes/gui/DeleteCharacterController.java
@@ -4,9 +4,24 @@
  */
 package brutes.gui;
 
+import brutes.ScenesContext;
+import brutes.net.Protocol;
+import brutes.net.client.ErrorResponseException;
+import brutes.net.client.InvalidResponseException;
+import brutes.net.client.NetworkClient;
+import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -14,7 +29,31 @@ import javafx.fxml.Initializable;
  * @author Karl
  */
 public class DeleteCharacterController implements Initializable {
-
+    @FXML
+    private void handleCancelAction(ActionEvent e){
+        this.closeStage(e);
+    }
+    
+    @FXML
+    private void handleSubmitAction(ActionEvent e){
+        try (NetworkClient connection = new NetworkClient(new Socket(ScenesContext.getInstance().getSession().getServer(), Protocol.CONNECTION_PORT))) {
+            try {
+                connection.sendCreateCharacter(ScenesContext.getInstance().getSession().getToken(), this.characterName.getText());
+            } catch (InvalidResponseException | ErrorResponseException ex) {
+                Logger.getLogger(FightController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ScenesContext.getInstance().getSession().netLoadMyCharacter();
+        this.closeStage(e);
+    }
+    
+    private void closeStage(Event e){
+        Node  source = (Node) e.getSource(); 
+        Stage stage  = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
     /**
      * Initializes the controller class.
      */
