69b5ab4358e3fc072af9a95f811cdf48fab185bd
Olivarès Georges <olivares.georges@gmail.com>
2013-01-23 02:28:40 +0100

    add Bonus Entity

--- src/brutes/game/Bonus.java
+++ src/brutes/game/Bonus.java
@@ -4,11 +4,14 @@
  */
 package brutes.game;
 
+import brutes.db.Entity;
+import java.sql.ResultSet;
+
 /**
  *
  * @author Karl
  */
-public class Bonus {
+public class Bonus implements Entity {
     public static final Bonus EMPTY_BONUS = new Bonus();
 
     private int id;
@@ -18,6 +21,10 @@ public class Bonus {
     private short speed;
     private int imageID;
 
+    public Bonus(ResultSet r) throws Exception {
+        this(r.getInt("id"), r.getString("name"), r.getShort("level"), r.getShort("strength"), r.getShort("speed"), r.getInt("id") /* TODO: change ID -> IMG */);
+    }
+    
     private Bonus(){
     }
     
