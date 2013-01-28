2b1faecf00e202901fd9bc338adb133c25171491
Olivarès Georges <olivares.georges@gmail.com>
2013-01-28 11:16:57 +0100

    add (boolean) ui.random()

--- src/brutes/server/ui.java
+++ src/brutes/server/ui.java
@@ -7,13 +7,18 @@ import brutes.server.db.DatasManager;
  * @author Thiktak
  */
 public class ui {
+
     public static int random(int min, int max) {
-        return (int) ( Math.random() * (max-min+1) + min );
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+    public static boolean random() {
+        return ui.random(0, 1) == 1;
     }
 
     public static String getClassPath(Class aClass) {
         String name = aClass.getName();
         String value = name.substring(0, name.lastIndexOf(DatasManager.class.getSimpleName()));
-        return value.substring(0, value.length()-1);
+        return value.substring(0, value.length() - 1);
     }
 }
